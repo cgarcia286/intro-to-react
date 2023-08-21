c015414e01f1b067b6433e627a061e107b1de21f
Carlos Garcia <carlos.garcia@clearreview.com>
2023-08-21 13:03:01 +0100

    Created basic components for todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './platzi.webp';
+import { CreateTodoButton } from './components/todo/Button';
+import { TodoCounter } from './components/todo/Counter';
+import { TodoItem } from './components/todo/Item';
+import { TodoList } from './components/todo/List';
+import { TodoSearch } from './components/todo/Search';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton />
     </div>
   );
 }
